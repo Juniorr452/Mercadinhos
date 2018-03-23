f9a4c8f8e14a47345ae0aaebabbec137ea45cd66
Junior <juniorr452@gmail.com>
2018-03-23 11:53:12 -0300

    Atualizando Firebase User

--- android/app/src/main/java/com/mobile/pid/pid/home/perfil/AtualizarPerfilActivity.java
+++ android/app/src/main/java/com/mobile/pid/pid/home/perfil/AtualizarPerfilActivity.java
@@ -6,6 +6,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.net.Uri;
 import android.provider.MediaStore;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -21,6 +22,9 @@ import android.widget.RadioButton;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.auth.UserProfileChangeRequest;
@@ -179,7 +183,7 @@ public class AtualizarPerfilActivity extends AppCompatActivity
                     imagemUri = data.getData();
 
                     Glide.with(this).load(imagemUri).into(imageView_user);
-                    Glide.with(this).load(imagemUri).into(imageView_user_blur);
+                    Glide.with(this).load(imagemUri).override(20,20).error(android.R.drawable.dark_header).into(imageView_user_blur);
                     break;
             }
         }
@@ -257,13 +261,28 @@ public class AtualizarPerfilActivity extends AppCompatActivity
         usuarioDatabaseRef.child("sexo").setValue(sexo);
         usuarioDatabaseRef.child("dataNascimento").setValue(dataNasc);
 
-        Toast.makeText(AtualizarPerfilActivity.this
-                , R.string.updated_data, Toast.LENGTH_SHORT).show();
-
         // Atualizar no Auth
+        UserProfileChangeRequest.Builder dadosPAtt = new UserProfileChangeRequest.Builder().setDisplayName(nome);
 
-        //user.updateProfile(new UserProfileChangeRequest());
+        if (imagemUri != null)
+            dadosPAtt = dadosPAtt.setPhotoUri(imagemUri);
 
-        finish();
+
+        user.updateProfile(dadosPAtt.build()).addOnCompleteListener(new OnCompleteListener<Void>() {
+            @Override
+            public void onComplete(@NonNull Task<Void> task) {
+                if (task.isSuccessful())
+                {
+                    Toast.makeText(AtualizarPerfilActivity.this
+                            , R.string.updated_data, Toast.LENGTH_SHORT).show();
+
+                    finish();
+                }
+                else
+                {
+                    Log.e(TAG, task.getException().toString());
+                }
+            }
+        });
     }
 }
