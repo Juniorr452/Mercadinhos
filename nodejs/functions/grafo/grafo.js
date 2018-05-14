768bbb8f6e8b35c6187673471b130a793144d1ba
Junior <juniorr452@gmail.com>
2018-05-14 17:55:29 -0300

    Pegando pessoas que o usuário está seguindo WIP

--- nodejs/functions/grafo/grafo.js
+++ nodejs/functions/grafo/grafo.js
@@ -1,18 +1,37 @@
 // Prototypes - https://tableless.com.br/dominando-o-uso-de-prototype-em-javascript/
 
-const node = require('./node.js');
+const vertice = require('./vertice.js');
+const admin   = require('firebase-admin');
 
-function Grafo(){
-    this.nodes = []; 
+function Grafo(profundidade)
+{
+    this.db = admin.database();
+
+    this.vertices     = [];
+    this.profundidade = profundidade;
+}
+
+Grafo.prototype.lerGrafo = function(uid)
+{
+    let seguindo = [];
+
+    const userSeguindoRef = this.db.ref("userSeguindo").child(uid);
+    
+    userSeguindoRef.once('value', snasphot => 
+    {
+        snasphot.forEach(usuario => {
+            console.log(usuario);
+        });
+    });
 }
 
 Grafo.prototype.helloWorldGrafo = function(){
     return "Hello World do Grafo";
 }
 
-/*Grafo.prototype.addNode(n)
+/*Grafo.prototype.addVertice(n)
 {
-    this.nodes.push(n);
+    this.vertices.push(n);
 }*/
 
 module.exports = Grafo;
--- nodejs/functions/index.js
+++ nodejs/functions/index.js
@@ -3,6 +3,7 @@
 
 const functions = require('firebase-functions');
 const Grafo     = require('./grafo/grafo.js');
+const admin     = require('firebase-admin');
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
@@ -11,11 +12,23 @@ const Grafo     = require('./grafo/grafo.js');
 //  response.send("Hello from Firebase!");
 // });
 
+admin.initializeApp();
+
 exports.olaMundoGrafo = functions.https.onRequest((request, response) => {
-    var grafo = new Grafo();
+    let grafo = new Grafo();
     response.send(grafo.helloWorldGrafo());
 });
 
-exports.olaMundo = functions.https.onRequest((request, response) => {
-    response.send('Hello World!');
+// Recomendações de usuários para seguir
+exports.getRecomendacoesUsuarios = functions.https.onRequest((request, response) => {
+    //let uid = request.query.uid;
+    let uid = "ECevzocmNxaERZq8KO3lwDiE79Y2";
+
+    if(uid === undefined)
+        return;
+
+    let grafo = new Grafo();
+    grafo.lerGrafo(uid);
+
+    response.send("uid = " + uid);
 });
