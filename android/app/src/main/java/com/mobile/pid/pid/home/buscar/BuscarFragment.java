23f81cea12fb08c7bb04e2b2c6b43a314ee4d7cd
Jonas Ramos <jjonasramos@hotmail.com>
2018-05-23 18:14:31 -0300

    Buscar sem sugestao de turmas

--- android/app/src/main/java/com/mobile/pid/pid/home/buscar/BuscarFragment.java
+++ android/app/src/main/java/com/mobile/pid/pid/home/buscar/BuscarFragment.java
@@ -46,9 +46,8 @@ public class BuscarFragment extends Fragment
 
     private TurmaAdapter turmaAdapter;
     private BuscarAdapter buscarAdapter;
-    private SugestaoAdapter sugestaoAdapter_turmas;
     private SugestaoAdapter sugestaoAdapter_usuarios;
-    private RecyclerView recyclerView_turmas;
+
     private RecyclerView recyclerView_usuarios;
     private RecyclerView recycle_busca;
     private Toolbar toolbar;
@@ -68,44 +67,9 @@ public class BuscarFragment extends Fragment
 
         setHasOptionsMenu(true);
 
-        turmasCriadas = new ArrayList<>();
-        turmaAdapter = new TurmaAdapter(getActivity(), turmasCriadas);
         buscarAdapter = new BuscarAdapter(getActivity());
 
-        sugestaoAdapter_turmas = new SugestaoAdapter(getActivity());
         sugestaoAdapter_usuarios = new SugestaoAdapter(getActivity());
-
-        turmasRef = FirebaseDatabase.getInstance().getReference().child("turmas");
-        turmasChildEventListener = new ChildEventListener() {
-            @Override
-            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                Turma t = dataSnapshot.getValue(Turma.class);
-                t.setId(dataSnapshot.getKey());
-                sugestaoAdapter_turmas.add(t);
-            }
-
-            @Override
-            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onChildRemoved(DataSnapshot dataSnapshot) {
-
-            }
-
-            @Override
-            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-
-            }
-
-            @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
-        };
-
-        turmasRef.addChildEventListener(turmasChildEventListener);
 
         FirebaseDatabase.getInstance().getReference("usuarios").addChildEventListener(new ChildEventListener() {
             @Override
@@ -145,16 +109,12 @@ public class BuscarFragment extends Fragment
         View v = inflater.inflate(R.layout.fragment_buscar, container, false);
 
         // Recycler View
-        recyclerView_turmas = v.findViewById(R.id.rv_turmas);
         recyclerView_usuarios = v.findViewById(R.id.rv_usuarios);
         recycle_busca = v.findViewById(R.id.recycle_busca);
         //searchView = v.findViewById(R.id.search_view);
 
-        LinearLayoutManager llm = new LinearLayoutManager(getActivity());
         LinearLayoutManager llm2 = new LinearLayoutManager(getActivity());
-        llm.setOrientation(LinearLayoutManager.HORIZONTAL);
         llm2.setOrientation(LinearLayoutManager.HORIZONTAL);
-        recyclerView_turmas.setLayoutManager(llm);
         recyclerView_usuarios.setLayoutManager(llm2);
         recycle_busca.setLayoutManager(new LinearLayoutManager(getActivity()));
 
@@ -162,7 +122,6 @@ public class BuscarFragment extends Fragment
         sugestoes = v.findViewById(R.id.sugestoes);
         busca = v.findViewById(R.id.busca);
 
-        recyclerView_turmas.setAdapter(sugestaoAdapter_turmas);
         recyclerView_usuarios.setAdapter(sugestaoAdapter_usuarios);
         recycle_busca.setAdapter(buscarAdapter);
 
