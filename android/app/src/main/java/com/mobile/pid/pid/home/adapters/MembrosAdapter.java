8620af7b34fae3fa2a6e88fc84307fe6cc54c5ec
Junior <juniorr452@gmail.com>
2018-06-10 20:55:33 -0300

    Membros fragment (Falta o click de excluir o aluno)

--- android/app/src/main/java/com/mobile/pid/pid/home/adapters/MembrosAdapter.java
+++ android/app/src/main/java/com/mobile/pid/pid/home/adapters/MembrosAdapter.java
@@ -3,13 +3,18 @@ package com.mobile.pid.pid.home.adapters;
 import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
+import com.bumptech.glide.Glide;
+import com.google.firebase.auth.FirebaseAuth;
 import com.mobile.pid.pid.R;
+import com.mobile.pid.pid.home.turmas.Turma;
 import com.mobile.pid.pid.login.Usuario;
 
 import java.util.ArrayList;
@@ -19,12 +24,20 @@ public class MembrosAdapter extends RecyclerView.Adapter<MembrosAdapter.MembrosV
 {
     private Context c;
     private List<Usuario> membros;
+    private Turma turma;
+    private Usuario professor;
 
-    public MembrosAdapter(Context c)
+    private String uidUsuarioLogado;
+
+    public MembrosAdapter(Context c, Turma turma)
     {
         this.c = c;
         this.membros = new ArrayList<>();
+        this.turma = turma;
+
+        this.uidUsuarioLogado = FirebaseAuth.getInstance().getCurrentUser().getUid();
     }
+
     @NonNull
     @Override
     public MembrosViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType)
@@ -39,8 +52,28 @@ public class MembrosAdapter extends RecyclerView.Adapter<MembrosAdapter.MembrosV
     public void onBindViewHolder(@NonNull MembrosViewHolder holder, int position)
     {
         Usuario membro = membros.get(position);
+        String     uid = membro.getUid();
 
+        if(turma.getProfessorUid().equals(uid))
+        {
+            holder.linearLayout.setBackgroundColor(c.getResources().getColor(R.color.colorPrimary));
+            holder.professor.setVisibility(View.VISIBLE);
+            Log.d("adskaskdas", "aaa" + uid);
+        }
 
+        Glide.with(c).load(membro.getFotoUrl()).into(holder.foto);
+
+        holder.nome.setText(membro.getNome());
+
+        if(!turma.getProfessorUid().equals(uidUsuarioLogado))
+            holder.excluir.setVisibility(View.GONE);
+        else
+            holder.excluir.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    // TODO: OnClick
+                }
+            });
     }
 
     @Override
@@ -48,15 +81,49 @@ public class MembrosAdapter extends RecyclerView.Adapter<MembrosAdapter.MembrosV
         return membros.size();
     }
 
+    public void setProfessor(Usuario professor){
+        this.professor = professor;
+    }
+
+    public void add(Usuario u){
+        membros.add(u);
+        notifyDataSetChanged();
+    }
+
+    public void setMembros(List<Usuario> membros){
+        this.membros = membros;
+
+        ordenarMembros();
+    }
+
+    public void clear(Usuario u){
+        membros.clear();
+        notifyDataSetChanged();
+    }
+
+    private void ordenarMembros()
+    {
+        membros.add(0, professor);
+        notifyDataSetChanged();
+    }
+
     public class MembrosViewHolder extends RecyclerView.ViewHolder
     {
+        public LinearLayout linearLayout;
         public ImageView foto;
         public TextView  nome;
         public TextView  professor;
         public ImageView excluir;
 
-        public MembrosViewHolder(View itemView) {
+        public MembrosViewHolder(View itemView)
+        {
             super(itemView);
+
+            linearLayout = itemView.findViewById(R.id.ll_membro);
+            foto         = itemView.findViewById(R.id.membro_foto);
+            nome         = itemView.findViewById(R.id.membro_nome);
+            professor    = itemView.findViewById(R.id.membro_professor);
+            excluir      = itemView.findViewById(R.id.membro_excluir);
         }
     }
 }
--- android/app/src/main/java/com/mobile/pid/pid/home/turmas/detalhes_turma/fragments/MembrosFragment.java
+++ android/app/src/main/java/com/mobile/pid/pid/home/turmas/detalhes_turma/fragments/MembrosFragment.java
@@ -2,17 +2,38 @@ package com.mobile.pid.pid.home.turmas.detalhes_turma.fragments;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.mobile.pid.pid.R;
+import com.mobile.pid.pid.home.adapters.MembrosAdapter;
+import com.mobile.pid.pid.home.turmas.Turma;
+import com.mobile.pid.pid.login.Usuario;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MembrosFragment extends Fragment
 {
+    DatabaseReference firebaseDatabase;
+    DatabaseReference alunosReference;
+    DatabaseReference usuariosReference;
+    DatabaseReference professorReference;
+
+    Turma   turma;
+    Usuario professor;
+
     RecyclerView recyclerView;
 
+    MembrosAdapter membrosAdapter;
 
     public MembrosFragment()
     {
@@ -23,6 +44,17 @@ public class MembrosFragment extends Fragment
     public void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
+
+        turma = (Turma) getArguments().getSerializable("turma");
+
+        firebaseDatabase   = FirebaseDatabase.getInstance().getReference();
+        alunosReference    = firebaseDatabase.child("turmas").child(turma.getId()).child("alunos");
+        usuariosReference  = firebaseDatabase.child("usuarios");
+        professorReference = usuariosReference.child(turma.getProfessorUid());
+
+        membrosAdapter = new MembrosAdapter(getContext(), turma);
+
+        pegarMembrosTurma();
     }
 
     @Override
@@ -30,6 +62,64 @@ public class MembrosFragment extends Fragment
     {
         View v = inflater.inflate(R.layout.fragment_membros, container, false);
 
+        recyclerView = v.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getContext()));
+        recyclerView.setAdapter(membrosAdapter);
+
         return v;
+    }
+
+    private void pegarMembrosTurma()
+    {
+        professorReference.addListenerForSingleValueEvent(new ValueEventListener()
+        {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                professor = dataSnapshot.getValue(Usuario.class);
+                professor.setUid(dataSnapshot.getKey());
+
+                membrosAdapter.setProfessor(professor);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+
+        alunosReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot)
+            {
+                final List<Usuario> alunos = new ArrayList<>(30);
+
+                for(final DataSnapshot data : dataSnapshot.getChildren())
+                {
+                    String alunoKey = data.getKey();
+
+                    usuariosReference.child(alunoKey).addListenerForSingleValueEvent(new ValueEventListener() {
+                        @Override
+                        public void onDataChange(DataSnapshot dataSnapshot)
+                        {
+                            Usuario aluno = dataSnapshot.getValue(Usuario.class);
+                            aluno.setUid(dataSnapshot.getKey());
+                            membrosAdapter.add(aluno);
+                            //.add(aluno);
+                        }
+
+                        @Override
+                        public void onCancelled(DatabaseError databaseError) {
+
+                        }
+                    });
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
     }
 }
